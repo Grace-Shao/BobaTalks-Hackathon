4b83be02ab5aa36a261a02035fc70d8983ba68f9
Grace-Shao <gshao8@gatech.edu>
2024-05-14 22:18:52 -0400

    can make a post request

--- src/pages/CreateEventPage.js
+++ src/pages/CreateEventPage.js
@@ -1,8 +1,26 @@
 import '../styles/CreateEventPage.css';
 import Navbar from '../components/Navbar';
 import Container from '@mui/material/Container';
+import axios from 'axios';
+import { useState } from 'react';
 
 export default function CreateEventPage() {
+    const [post, setPost] = useState({eventName: '', eventDescription: '', goalAmount: '', numOfPeople: '', uploadPhotos: ''})
+    const createEvent = async (event) => {
+        event.preventDefault();
+        axios.post('http://localhost:5000/events', post)
+            .then(response => {
+                console.log(response.data);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }
+    const handleChange = (event) => {
+        // form input element that the user is interacting with
+        const {name, value} = event.target;
+        setPost(prevPost => ({...prevPost, [name]: value}));
+    }
     return (
         <div>
             <Navbar />
@@ -15,22 +33,25 @@ export default function CreateEventPage() {
                 pb: { xs: 8, sm: 12 },
                 }}
             >
-            <h1>RecieverPage</h1>
-            <form>
+            <h1>Create an Event</h1>
+            <form onSubmit={createEvent}>
                 <label htmlFor="eventName">Event Name:</label>
-                <input type="text" id="eventName" name="eventName" />
+                <input type="text" id="eventName" name="eventName" onChange={handleChange} />
+
+                <label htmlFor="eventOwner">Event Owner:</label>
+                <input type="text" id="eventOwner" name="eventOwner" onChange={handleChange} />
 
                 <label htmlFor="eventDescription">Event Description:</label>
-                <input type="text" id="eventDescription" name="eventDescription" />
+                <input type="text" id="eventDescription" name="eventDescription" onChange={handleChange} />
 
                 <label htmlFor="goalAmount">Goal Amount:</label>
-                <input type="number" id="goalAmount" name="goalAmount" />
+                <input type="number" id="goalAmount" name="goalAmount" onChange={handleChange} />
 
-                <label htmlFor="numOfPeople">Number of People:</label>
-                <input type="number" id="numOfPeople" name="numOfPeople" />
+                {/* <label htmlFor="numOfPeople">Number of People:</label>
+                <input type="number" id="numOfPeople" name="numOfPeople" onChange={handleChange} /> */}
                 
-                <label htmlFor="uploadPhotos">Upload Photos:</label>
-                <input type="file" id="uploadPhotos" name="uploadPhotos" accept="image/*" multiple />
+                {/* <label htmlFor="uploadPhotos">Upload Photos:</label>
+                <input type="file" id="uploadPhotos" name="uploadPhotos" accept="image/*" multiple onChange={handleChange} /> */}
 
                 <button type="submit">Create Campaign</button>
             </form>
