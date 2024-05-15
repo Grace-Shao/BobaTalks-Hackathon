bdade8ffcacd53a93d63c7357b3dbebf19c9d9a6
NatashaSetiadi <natset456@gmail.com>
2024-05-15 11:31:11 -0400

    Implement styling on SignIn page

--- src/components/SignInForm.js
+++ src/components/SignInForm.js
@@ -33,7 +33,7 @@ export default function SignInForm() {
           display: 'flex',
           flexDirection: 'column',
           alignItems: 'center',
-          pt: { xs: 14, sm: 20 },
+          pt: { xs: 11, sm: 11},
           pb: { xs: 8, sm: 12 },
           // px: {xs: 80, sm:80},
           maxWidth: 0,
@@ -41,17 +41,17 @@ export default function SignInForm() {
           bgcolor: '#EDAB6F',
         }}
       >
-
+        <span className="circle"></span>
         <Typography sx={{fontFamily: "Poppins", padding: 2, color:'#021944', fontWeight: 'bold'}} variant="h4" component="div">
           Enter account details
         </Typography>
-        <TextField sx={{mt: 1, border: '2px solid', borderColor: 'black', background: '#FFFFFF'}} 
+        <TextField sx={{mt: 1, border: '2px solid', borderColor: 'black', background: '#FFFFFF', width: 500}} 
         id="outlined-basic" label="Full Name" variant="outlined" />
-        <TextField sx={{mt: 1, border: '2px solid', borderColor: 'black', background: '#FFFFFF'}} 
+        <TextField sx={{mt: 1, border: '2px solid', borderColor: 'black', background: '#FFFFFF', width: 500}} 
         id="outlined-basic" label="Email Address" variant="outlined" />
-        <TextField sx={{mt: 1, border: '2px solid', borderColor: 'black', background: '#FFFFFF'}} 
+        <TextField sx={{mt: 1, border: '2px solid', borderColor: 'black', background: '#FFFFFF', width: 500}} 
         id="outlined-basic" label="Password" variant="outlined" />
-        <TextField sx={{mt: 1, border: '2px solid', borderColor: 'black', background: '#FFFFFF'}} 
+        <TextField sx={{mt: 1, border: '2px solid', borderColor: 'black', background: '#FFFFFF', width: 500}} 
         id="outlined-basic" label="Confirm Password" variant="outlined" />
         <Button sx={{mt: 1}} color="custom" variant="outlined" href="/SignIn"> Register </Button>
 
