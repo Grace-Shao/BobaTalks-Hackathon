4cea5f6455e9d07fdbdf9394e4419cbfb491cd35
alexander shen <alexander.shen@gmail.com>
2024-05-16 18:41:24 -0400

    made display changes to All Events page to match figma behavior and design more closely

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -5,22 +5,35 @@ export default function EventCard({event}) {
     const [isExpanded, setIsExpanded] = useState(false);
     return (
         <div className="card">
-            <h1>{event.event_name}</h1>
-            <p>Organized by: {event.event_owner}</p>
-            <p>{event.event_description}</p>
-            <p>Goal: ${event.goal_amount}</p>
-            <p>Current: ${event.current_money}</p>
-            <p>End Date: {event.end_date.toISOString().substring(0, 10)}</p>
-            <button onClick={() => setIsExpanded(!isExpanded)}>Learn More</button>
-            {/*conditional render this*/}
-            {isExpanded && (
-                <>
-                    <p>{event.event_description}</p>
+            <div>
+                <h1 class="card_event-name">{event.event_name}</h1>
+                <p class="card_organization">Organized by: {event.event_owner}</p>
+                <p class="card_goal">Goal Amount: ${event.goal_amount}</p>
+                {/*conditional render this*/}
+                {isExpanded && (
+                    <>
+                        <p class="text-lg">Amount Raised: ${event.current_money}</p>
+                        <p>{event.event_description}</p>
+                    </>
+                )}
+            </div>
+            <div class="card_right-div">
+                <p>End Date: {event.end_date.toISOString().substring(0, 10)}</p>
+                {!isExpanded && (
+                    <button 
+                        onClick={() => setIsExpanded(!isExpanded)}
+                        class="card_learn-more">
+                        Learn More</button>
+                )}
+                
+                {isExpanded && (
                     <Link to={`DonatePage/${event._id}`}>
                         <button className="donateButton">Donate</button>
                     </Link>
-                </>
-            )}
+                )}
+            </div>
+                                    
+            
         </div>
     );
 }
