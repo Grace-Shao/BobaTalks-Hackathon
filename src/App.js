39a1695c990a0c5e09c215bfa2aaea83dea590af
Grace-Shao <gshao8@gatech.edu>
2024-05-16 21:13:55 -0400

    vendors page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import EventsPage from './pages/EventsPage';
 import CreateEventPage from './pages/CreateEventPage';
 import Home from './pages/Home';
 import DonatePage from './pages/DonatePage';
+import BobaVendorsPage from './pages/BobaVendorsPage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
@@ -15,6 +16,7 @@ function App() {
         <Route path="/EventsPage" element={<EventsPage/>}/>
         <Route path="/EventsPage/DonatePage/:id" element={<DonatePage/>}/>
         <Route path="/CreateEventPage" element={<CreateEventPage/>}/>
+        <Route path="/BobaVendorsPage" element={<BobaVendorsPage/>}/>
         <Route path="/SignIn" element={<SignIn/>}/>
         <Route path="/SignUp" element={<SignUp/>}/>
       </Routes>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -114,15 +114,20 @@ function Navbar() {
                   to="/CreateEventPage"
                   sx={{ py: '6px', px: '12px' }}
                 >
-
                   <h4 style={{fontFamily: "Poppins", color:'white', fontWeight: 'normal'}}>Create Event</h4>
                 </MenuItem>
                 <MenuItem
                   onClick={() => scrollToSection('testimonials')}
                   sx={{ py: '6px', px: '12px' }}
                 >
-
                   <h4 style={{fontFamily: "Poppins", color:'white', fontWeight: 'normal'}}>Manage Event</h4>
+                </MenuItem>
+                <MenuItem
+                  component={Link}
+                  to="/BobaVendorsPage"
+                  sx={{ py: '6px', px: '12px' }}
+                >
+                  <h4 style={{fontFamily: "Poppins", color:'white', fontWeight: 'normal'}}>See Boba Vendors</h4>
                 </MenuItem>
               </Box>
             </Box>
