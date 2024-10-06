adfaab47ffab0688e761289b3b485aef22a85432
alexander shen <alexander.shen@gmail.com>
2024-10-05 22:45:45 -0400

    removed theme code from SignInForm

--- src/components/SignInForm.js
+++ src/components/SignInForm.js
@@ -3,21 +3,10 @@ import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 import '../styles/style.css';
 import { useAuth } from '../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
 
-const theme = createTheme({
-  palette: {
-    custom: {
-      main: '#FFFFFF',
-      light: '#FFFFFF',
-      dark: '#FFFFFF',
-      contrastText: '#FFFFFF',
-    },
-  },
-});
 
 export default function SignInForm() {
   const { login } = useAuth();
@@ -32,39 +21,37 @@ export default function SignInForm() {
   };
 
   return (
-    <ThemeProvider theme={theme}>
-      <Container
-      className = "width-no-space"
+    <Container
+      className="width-no-space"
       disableGutters
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          pt: { xs: 11, sm: 11},
-          pb: { xs: 8, sm: 12 },
-          // px: {xs: 80, sm:80},
-          maxWidth: 0,
-          width: '100%',
-          bgcolor: '#EDAB6F',
-          minHeight: '100vh',
-        }}
-      >
-        <span className="circle"></span>
-        <Typography sx={{fontFamily: "Poppins", padding: 2, color:'#021944', fontWeight: 'bold'}} variant="h4" component="div">
-          Enter account details
-        </Typography>
-        <form onSubmit={handleSubmit}>
-          <TextField 
-            sx={{mt: 1, border: '2px solid', borderColor: 'black', background: '#FFFFFF', width: 500}} 
-            id="outlined-basic" label="Email Address" variant="outlined" 
-            value={email} onChange={e => setEmail(e.target.value)} />
-          <TextField 
-            sx={{mt: 1, border: '2px solid', borderColor: 'black', background: '#FFFFFF', width: 500}} 
-            id="outlined-basic" label="Password" variant="outlined" 
-            value={password} onChange={e => setPassword(e.target.value)} />
-          <Button sx={{mt: 1}} color="custom" variant="outlined" onClick={handleSubmit}> Login </Button>
-        </form>
-      </Container>
-      </ThemeProvider>
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        pt: { xs: 11, sm: 11 },
+        pb: { xs: 8, sm: 12 },
+        // px: {xs: 80, sm:80},
+        maxWidth: 0,
+        width: '100%',
+        bgcolor: '#EDAB6F',
+        minHeight: '100vh',
+      }}
+    >
+      <span className="circle"></span>
+      <Typography sx={{ fontFamily: "Poppins", padding: 2, color: '#021944', fontWeight: 'bold' }} variant="h4" component="div">
+        Enter account details
+      </Typography>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          sx={{ mt: 1, border: '2px solid', borderColor: 'black', background: '#FFFFFF', width: 500 }}
+          id="outlined-basic" label="Email Address" variant="outlined"
+          value={email} onChange={e => setEmail(e.target.value)} />
+        <TextField
+          sx={{ mt: 1, border: '2px solid', borderColor: 'black', background: '#FFFFFF', width: 500 }}
+          id="outlined-basic" label="Password" variant="outlined"
+          value={password} onChange={e => setPassword(e.target.value)} />
+        <Button sx={{ mt: 1 }} color="custom" variant="outlined" onClick={handleSubmit}> Login </Button>
+      </form>
+    </Container>
   );
 }
